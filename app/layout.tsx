eb1400b254675c6c41cc911392b6e80f3681333c
Grace Ng <grace@leanstartupmachine.com>
2023-11-29 15:57:42 -0800

    helmet sync link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Mimes.ai - Make Your Films',
   description: 'Create 3D Characters and Scenes by Typing',
-};
-
-interface RootLayoutProps {
-  children: React.ReactNode; 
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-      </Head>
-      <body className={inter.className}>
-        {children}
-      </body>
-    </>
-  );
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
--- components/Email.tsx
+++ components/Email.tsx
@@ -1,26 +1,23 @@
-"use client";
-import { Helmet } from 'react-helmet';
+// Note: React Helmet is not the recommended way to insert scripts in Next.js.
+// Use the built-in Script component instead.
+import Script from 'next/script';
 
 export default function Email() {
-return (
-    
- 
-<div>
-    <div
-            id="getWaitlistContainer"
-            data-waitlist_id="12159"
-            data-widget_type="WIDGET_2"
-            placeholder='Enter your email'
-            className='class="flex w-full flex-col justify-center items-center'
-        ></div>
-        <Helmet>
+  return (
+    <div>
+      <div
+        id="getWaitlistContainer"
+        data-waitlist_id="12159"
+        data-widget_type="WIDGET_2"
+        className="flex w-full flex-col justify-center items-center"
+        placeholder="Enter your email"
+      ></div>
 
-            <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
-        </Helmet>
-</div>
-
-
-
-
-    );
+      {/* Next.js Script component with async attribute */}
+      <Script
+        src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"
+        strategy="afterInteractive"
+      />
+    </div>
+  );
 }
